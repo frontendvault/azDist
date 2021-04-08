19f2094da4c23227fd7e6e89eb777b519fa31348
manoj pathania <manoj07.pathania@gmail.com>
2021-04-08 22:52:01 +0530

    cart dropdown

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,16 @@
-const changeView = document.querySelector("change-view");
+// const changeView = document.querySelector("change-view");
 
-if (changeView.classList.contains === "normal-view") {
-  changeView.classList.remove("collapse-view");
-} else {
-  changeView.classList.add("collapse-view");
+// if (changeView.classList.contains === "normal-view") {
+//   changeView.classList.remove("collapse-view");
+// } else {
+//   changeView.classList.add("collapse-view");
+// }
+
+const block = document.querySelector("blocks-button");
+const stackView = document.querySelector("stack-view");
+
+block.addEventListener("click", blockedContent);
+
+function blockedContent() {
+  stackView.classList.add("d-none");
 }
